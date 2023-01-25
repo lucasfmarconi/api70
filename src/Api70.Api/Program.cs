884d9816b45d16a541da57f0635742427c228573
Lucas F Marçoni Ferreira <lucasfmarconi@gmail.com>
2023-01-25 19:52:05 +0100

    Configure serilog

--- src/Api70.Api/Program.cs
+++ src/Api70.Api/Program.cs
@@ -1,36 +1,56 @@
-using static Microsoft.AspNetCore.Builder.WebApplication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using static Microsoft.AspNetCore.Builder.WebApplication;
 
-internal class Program
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
+try
 {
-    private static void Main(string[] args)
+    var builder = CreateBuilder(args);
+
+    builder.Host.UseSerilog((hosting, logging) =>
     {
-        var builder = CreateBuilder(args);
+        logging.Enrich.WithProperty("ApplicationId", Guid.NewGuid());
+        logging.ReadFrom.Configuration(hosting.Configuration);
+    });
 
-        // Add services to the container.
+    // Add services to the container.
 
-        builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 
-        var app = builder.Build();
+    var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        app.Run();
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
     }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.UseSerilogRequestLogging();
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+    Console.ReadKey();
 }
