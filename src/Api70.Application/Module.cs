f56e41c078a3991a971e84cbfb13f80562700fd1
Lucas F Marçoni Ferreira <lucasfmarconi@gmail.com>
2023-01-25 18:30:47 +0100

    Fix project reference

--- src/Api70.Application/Module.cs
+++ src/Api70.Application/Module.cs
@@ -1,5 +1,10 @@
-﻿namespace Api70.Application;
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Api70.Application;
 public static class Module
 {
-
+    public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
+    {
+        return services;
+    }
 }
